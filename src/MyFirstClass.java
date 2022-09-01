949b5ce6f17d3802926ed96dd0ed6a2971a3f928
Sam Black <sam.black@lubbockisd.org>
2022-09-01 09:19:59 -0500

    Added spaces and additional imperative

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,6 +1,7 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.print("Greetings programs!");
+        System.out.print("Greetings programs!  ");
         System.out.println("Welcome to The Grid!");
+        System.out.println("Grab your ducks!");
     }
 }
